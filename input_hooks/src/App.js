ba31179b3ba4ca099a514d9e713ab1f7fd14a5d1
dave <dave@daves-MacBook-Pro.local>
2019-04-12 15:33:26 -0400

    extract handleNameChange

--- input_hooks/src/App.js
+++ input_hooks/src/App.js
@@ -2,42 +2,34 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App(props) {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-
-  function handleFirstNameChange(e) {
-    setFirstName(e.target.value);
-  }
-
-  function handleLastNameChange(e) {
-    setLastName(e.target.value);
-  }
+  const firstName = useFormInput("Dave");
+  const lastName = useFormInput("Spring");
 
   return (
     <React.Fragment>
       <div>
         First Name: <br />
-        <input
-          type="text"
-          name="firstName"
-          value={firstName}
-          onChange={handleFirstNameChange}
-        />
+        <input {...firstName} />
         <br />
         <h4>{firstName}</h4>
         <br />
         Last Name: <br />
-        <input
-          type="text"
-          name="lastName"
-          value={lastName}
-          onChange={handleLastNameChange}
-        />
+        <input {...lastName} />
         <br />
         <h4>{lastName}</h4>
       </div>
     </React.Fragment>
   );
+}
+
+function useFormInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
+  return { value, onChange: handleChange };
 }
 
 export default App;
