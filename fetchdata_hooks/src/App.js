f12be5c8cec92f1e8dc24ed2d1f3a0b034067e73
dave <dave@daves-MacBook-Pro.local>
2019-04-13 07:40:54 -0400

    Add fetch data with UseEffect hook

--- fetchdata_hooks/src/App.js
+++ fetchdata_hooks/src/App.js
@@ -1,8 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App(props) {
-  return <React.Fragment>hello</React.Fragment>;
+  const [data, setData] = useState({ hits: [] });
+
+  useEffect(() => {
+    // Must use async inside of useEffect
+    const fetchData = async () => {
+      const result = await axios(
+        "http://hn.algolia.com/api/v1/search?query=redux"
+      );
+      console.log(result);
+      setData(result.data);
+    };
+
+    fetchData();
+  }, []); // Use empty array here to fetch data only on mounting
+
+  return (
+    <React.Fragment>
+      <ul>
+        {data.hits.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    </React.Fragment>
+  );
 }
 
 export default App;
